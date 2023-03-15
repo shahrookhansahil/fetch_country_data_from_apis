9ce141bbc4390664a1de5a01ab9eb06ef6f51b73
shahrookhansahil <shahroo.bscsf19@iba-suk.edu.pk>
2023-03-15 18:32:45 +0500

    Unused data deleted

--- src/App.js
+++ src/App.js
@@ -10,28 +10,29 @@ function App() {
       fetch('https://restcountries.com/v2/all?fields=name,region,area') //Fetching data from given Api
       .then(response =>response.json())
       .then(json => setData(json))
-      .catch(e=>{
-        console.log("Error", e)
+      .catch(error=>{
+        console.log("Error", error)
       })
     }
     fetchData();
   },[])
   const filter_by_Oceania = () => {
-    const filtered = data.filter(function(ele){
-      return ele.region=="Oceania";
+    const filtered = data.filter(function(element){
+      return element.region=="Oceania";
   });
     setData(filtered);
   };
   const filter_by_Lithuania = () => {
     const filtered = data
-    .filter(function(ele){
-      return ele.area<=65300;
-  });
-    setData(filtered);
-    
+    .filter(function(element){
+      return element.area<=65300;
+    });
+    setData(filtered);    
   };
+
+  //Sort Function used to sort Data By Name
   const Sort_Data = () => {
-    const sorted = data.slice().reverse()
+    const sorted = data.slice().reverse()  //slice() operation clones the array and returns a reference to a new array
     setData(sorted);
   };
   
--- src/Components/Elements.jsx
+++ src/Components/Elements.jsx
@@ -1,9 +1,6 @@
 import React from 'react'
-// import {useState} from 'react'
 import './Element.css';
 const Elements = (props) => {
-  // const [data ,setData] = useState(props)
-  // console.log(props.name)
   return (
     <div  id='elmnt'>
         <div><b>{props.name}</b></div>
